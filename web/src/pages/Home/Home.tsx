2fb430224b5acca837190d965a859975de7f9a22
Gabriela <gabrielabuilcl@gmail.com>
2024-08-17 09:26:17 +0200

    PD-8695epft3 : Component button created and tested

--- web/src/pages/Home/Home.tsx
+++ web/src/pages/Home/Home.tsx
@@ -1,8 +1,12 @@
-// import React from 'react';
+import Button from '../../components/Button/Button';
 import styles from './Home.module.css';
 import headerImage from '../../assets/Playa.webp';
 
 const Home = () => {
+    const handleClick = () => {
+        console.log('Button clicked!');
+    };
+
     return (
         <main>
             <div className={styles.container_header_img}>
@@ -11,6 +15,13 @@ const Home = () => {
             <div className={styles.section}>
                 <div>Home</div>
             </div>
+            <Button label="Click me" onClick={handleClick} />
+            <Button label="aloha" onClick={handleClick} />
+            <Button label="bon dia" onClick={handleClick} />
+            <Button label="bona tarda" onClick={handleClick} />
+            <Button label="pinguino" onClick={handleClick} />
+            <Button label="zebra" onClick={handleClick} />
+            <Button label="luckiluk" onClick={handleClick} />
         </main>
     );
 };
