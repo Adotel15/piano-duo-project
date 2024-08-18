21b2b647932ab7de570de25d00e3516d1a7778e2
Gabriela <gabrielabuilcl@gmail.com>
2024-08-19 01:02:54 +0200

    PD-8695exrjm : Menu component files created and MenuOptions function in Menu.txs done

---
+++ web/src/components/Menu/Menu.tsx
@@ -0,0 +1,32 @@
+
+export const menuOptions = [
+    {
+        id: 0,
+        label: 'Inicio',
+        path: '/home',
+
+    },
+    {
+        id: 1,
+        label: 'Reviews',
+        path: '/reviews',
+
+    },
+    {
+        id: 2,
+        label: 'Contacto',
+        path: '/contact',
+
+    },
+    {
+        id: 3,
+        label: 'Media',
+        path: '/media',
+
+    }
+];
+
+// export default function List () {
+//     const listItems = menuOptions.map(menuOption => <li>{menuOption}</li>);
+//     return <ul>{listItems}</ul>;
+// }
