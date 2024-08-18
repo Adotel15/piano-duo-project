6763a251143def57f153c374ca3b6f0ff9bb9c22
Gabriela <gabrielabuilcl@gmail.com>
2024-08-18 19:22:45 +0200

    PD-8695ext00 - WIP, Reviews.txs with fetch from strapi but not tested

--- web/src/pages/Reviews/Reviews.tsx
+++ web/src/pages/Reviews/Reviews.tsx
@@ -1,7 +1,29 @@
 import './Reviews.module.css';
+import { useState, useEffect } from 'react';
 
 const Reviews = () => {
-    return <div>Reviews</div>;
+    const [reviews, setReviews] = useState([]);
+    useEffect (() => {
+        fetch('https://strapi.io/')
+            .then(res => {
+                return res.json();
+            })
+            .then(data => {
+                console.log(data);
+                setReviews(data);
+            });
+    }, []);
+
+    return (
+        <div>
+            <div>Reviews</div>
+            {reviews.map(review =>
+                <div>Reviews: {review}</div>
+            )}
+
+        </div>
+
+    );
 };
 
 export default Reviews;
