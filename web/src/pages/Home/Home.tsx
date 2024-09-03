d077e4023637f971ba44847fa2dd173cc61417c3
Gabriela <gabrielabuilcl@gmail.com>
2024-09-03 20:25:17 +0200

    PD-8695gk3rm : bug fixed of hero image

--- web/src/pages/Home/Home.tsx
+++ web/src/pages/Home/Home.tsx
@@ -1,12 +1,12 @@
 
 import styles from './Home.module.css';
-// import headerImage from '../../assets/Playa.webp';
+import headerImage from '../../assets/Playa.webp';
 
 const Home = () => {
     return (
         <main>
             <div className={styles['container_header_img']}>
-                {/* <img className={styles['header_img']} src={headerImage} alt="Playa" /> */}
+                <img className={styles['header_img']} src={headerImage} alt="beach" />
             </div>
             <div className={styles['section']}>
                 <div>Home</div>
