10463231f07847cfe9d51f9898b15d17f9c60587
Gabriela <gabrielabuilcl@gmail.com>
2024-09-10 23:36:47 +0200

    PD-8695ext00 : added useState(), fetch() fu > ition and map() function in Reviews.tsx. reviews api localHost is not working.

--- web/src/pages/Reviews/Reviews.tsx
+++ web/src/pages/Reviews/Reviews.tsx
@@ -1,13 +1,32 @@
 import Menu from '../../components/Menu/Menu';
 import './Reviews.module.css';
+import { useState } from 'react';
 
 const Reviews = () => {
-    return (
+    const [reviews, setReviews] = useState([]);
+
+    const getReviews = async () => {
+        const response = await fetch('http://localhost:8081/v1/api/reviews');
+        const data = await response.json();
+        setReviews(data);
+    };
+
+    getReviews();
+
+    return <div>
+        Reviews
         <div>
             <Menu />
-            <div>Reviews</div>
+            {reviews.map(review => {
+                return <div key={review.id}>
+                    <h2>{review.title}</h2>
+                    <p>{review.content}</p>
+                    <p>{review.reviewer}</p>
+                    <img src={review.image} alt={review.title} />
+                </div>;
+            })}
         </div>
-    );
+    </div>;
 };
 
 export default Reviews;
