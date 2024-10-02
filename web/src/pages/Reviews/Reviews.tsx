7a5497a87431e87195c91c1be80c5be819c4f536
Gabriela <gabrielabuilcl@gmail.com>
2024-10-02 15:32:15 +0200

    PD-8695ext00 : fetch debugged in  Reviews.tsx, waiting for debug in API to see the resoults in web)

--- web/src/pages/Reviews/Reviews.tsx
+++ web/src/pages/Reviews/Reviews.tsx
@@ -1,33 +1,46 @@
 import Menu from '../../components/Menu/Menu';
 import './Reviews.module.css';
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+type Review = {
+    id : string,
+    title: string,
+    content: string,
+    reviewer: string,
+    image: string,
+}
 
 const Reviews = () => {
-    const [reviews, setReviews] = useState([]);
+    const [reviews, setReviews] = useState <Review[]> ([]);
+    useEffect(() => {
+        getReviews();
+    }, []
+    );
 
     const getReviews = async () => {
         const response = await fetch('http://localhost:8081/v1/api/reviews');
         const data = await response.json();
-        setReviews(data);
+        setReviews(data.data);
     };
-
-    getReviews();
-
-    return <div>
-        Reviews
+    return (
         <div>
-            <Menu />
-            {reviews.map(review => {
-                return <div key={review.id}>
-                    <h2>{review.title}</h2>
-                    <p>{review.content}</p>
-                    <p>{review.reviewer}</p>
-                    <img src={review.image} alt={review.title} />
-                </div>;
-            })}
+            <h1>Reviews</h1>
+            <div>
+                <Menu />
+                {reviews.map(review => {
+                    return (
+                        <section key={review.id}>
+                            <h2>{review.title}</h2>
+                            <p>{review.content}</p>
+                            <p>{review.reviewer}</p>
+                            <img src={review.image} alt={review.title} />
+                        </section>
+                    );
+                })}
+            </div>
         </div>
-    </div>;
+    );
 };
 
 export default Reviews;
