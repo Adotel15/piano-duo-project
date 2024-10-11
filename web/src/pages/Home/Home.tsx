cb392d0550ed1d3815fe86242b40ec81a7601dde
Gabriela <gabrielabuilcl@gmail.com>
2024-10-11 11:12:15 +0200

    PD-869633fv7 Home page matches Figma, added Home Image and Logo, changed background

--- web/src/pages/Home/Home.tsx
+++ web/src/pages/Home/Home.tsx
@@ -1,16 +1,17 @@
 import styles from './Home.module.css';
-import headerImage from '../../assets/Playa.webp';
-
-// import Menu from '../../components/Menu/Menu';
+import headerImage from '../../assets/HomePD.png';
+import logoImage from '../../assets/Logo.png';
 
 const Home = () => {
     return (
         <main>
-            <div className={styles['container_header_img']}>
-                <img className={styles['header_img']} src={headerImage} alt="beach" />
-            </div>
             <div className={styles['section']}>
-                <div>Home</div>
+                <div>
+                    <img src={logoImage} alt='Logo'/>
+                </div>
+            </div>
+            <div className={styles['container_header_img']}>
+                <img className={styles['header_img']} src={headerImage} alt="Pianists" />
             </div>
         </main>
     );
