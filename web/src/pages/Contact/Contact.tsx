7970ed4783d76b5f79cb636daf7cb87d1da8930f
Gabriela <gabrielabuilcl@gmail.com>
2024-10-18 20:00:24 +0200

    PD-86968t94c contact page matching design, form and picture added. some buggs in form section

--- web/src/pages/Contact/Contact.tsx
+++ web/src/pages/Contact/Contact.tsx
@@ -1,13 +1,42 @@
 import Menu from '../../components/Menu/Menu';
-import './Contact.module.css';
+import styles from './Contact.module.css';
+import imageContact from '../../assets/Contact/Concerts_017.png';
 
 const Contact = () => {
     return (
         <div>
-            <h1>Contacto</h1>
             <div>
                 <Menu />
             </div>
+            <section className={styles['section-contact']}>
+                <div className={styles['container-image-Contact']}>
+                    <img className={styles['image-Contact']} src={imageContact} alt="Pianists"/>
+                </div>
+                <div className={styles['div-form']}>
+                    <h1>—Contacto</h1>
+                    <form className={styles['container-form-contact']}>
+                        <label htmlFor="name">Nombre</label>
+                        <input className={styles['input-form']} id="name" type="text" />
+
+                        <label htmlFor="email">Correo electrónico</label>
+                        <input className={styles['input-form']} id="email" type="text" />
+
+                        <label htmlFor="subject">Asunto</label>
+                        <input className={styles['input-form']} id="subject" type="text" />
+
+                        <label htmlFor="message">Mensaje
+                            <textarea className={styles['input-form']} id="message" rows={7}></textarea>
+                        </label>
+
+                        <label htmlFor="checkbox" className="checkbox-label">
+                            <input type="checkbox" id="checkbox" />
+                            He leído y acepto los <span>términos y condiciones</span> y acepto la <span>política de privacidad</span>
+                        </label>
+
+                        <input type="submit" value="Submit"/>
+                    </form>
+                </div>
+            </section>
         </div>
     );
 };
