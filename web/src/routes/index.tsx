8d1405f54cbd557c7aa59e5e211c50fceead347e
Gabriela <gabrielabuilcl@gmail.com>
2024-10-18 11:41:19 +0200

    PD-86968hbnn index routes updated

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -4,6 +4,10 @@ import Contact from '../pages/Contact/Contact';
 import Home from '../pages/Home/Home';
 import Media from '../pages/Media/Media';
 import Reviews from '../pages/Reviews/Reviews';
+import Gallery from '../pages/Gallery/Gallery';
+import Music from '../pages/Music/Music';
+import Repertoire from '../pages/Repertoire/Repertoire';
+import Biography from '../pages/Biography/Biography';
 
 const AppRoutes = () =>
     <BrowserRouter>
@@ -12,6 +16,10 @@ const AppRoutes = () =>
             <Route path="media" element={<Media />} />
             <Route path="reviews" element={<Reviews />} />
             <Route path="contact" element={<Contact />} />
+            <Route path="gallery" element={<Gallery />}/>
+            <Route path="music" element={<Music />}/>
+            <Route path="Repertoire" element={<Repertoire />}/>
+            <Route path="Biography" element={<Biography />}/>
         </Routes>
     </BrowserRouter>
 ;
