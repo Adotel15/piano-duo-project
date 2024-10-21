18302c2d1badc9955d0a1b2e8101b21a049c83a9
Gabriela <gabrielabuilcl@gmail.com>
2024-10-21 18:40:26 +0200

    PD-86968z0xt : navbar added to gallery to test. Navbar images added in Assets, and added in Navbar. Menu VERY BUGG

--- web/src/components/Navbar/NavBar.tsx
+++ web/src/components/Navbar/NavBar.tsx
@@ -1,8 +1,26 @@
+import Menu from '../Menu/Menu';
+import styles from './Navbar.module.css';
+//assets
+import musicOnOff from '../../assets/Navbar/logoMusic.png';
+import NamesImage from '../../assets/Navbar/Names.png';
+
 const Navbar = () => {
     return (
-        <div>
-
-        </div>
+        <>
+            <div className={styles['navbar-container']}>
+                <div className={styles['namesImage-container']}>
+                    <img src={NamesImage} className={styles['namesImage']} alt="names" />
+                </div>
+                {/* <section className={styles['navbarRight-container']}> */}
+                <div className={styles['musicOnOff-container']}>
+                    <img src={musicOnOff} alt="Music_ON/OFF" />
+                </div>
+                {/* <div className={styles['menu-container']}> */}
+                {/* </div>
+            </section> */}
+            </div>
+            <Menu />
+        </>
     );
 };
 export default Navbar;
--- web/src/pages/Gallery/Gallery.tsx
+++ web/src/pages/Gallery/Gallery.tsx
@@ -1,13 +1,12 @@
 import './Gallery.module.css';
-import Menu from '../../components/Menu/Menu';
+
+import Navbar from '../../components/Navbar/NavBar';
 
 const Gallery = () => {
     return (
         <div>
+            <Navbar/>
             <h1>Galería</h1>
-            <div>
-                <Menu />
-            </div>
         </div>
     );
 };
