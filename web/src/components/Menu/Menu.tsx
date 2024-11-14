2c83634d210e07a1b833a5f32965e6dfcd6e7600
Gabriela <gabrielabuilcl@gmail.com>
2024-11-14 18:24:44 +0100

    PD-86968z0xt : menuOpen and MenuClosed images added but it has buggs

--- web/src/components/Menu/Menu.tsx
+++ web/src/components/Menu/Menu.tsx
@@ -2,6 +2,8 @@ import { Link } from 'react-router-dom';
 import styles from './Menu.module.css';
 import { useState } from 'react';
 import { menuOptions as defaultMenuOptions } from '../../constants/ui';
+import MenuOpen from '../../assets/Menu/healthicons_ui-menu.png';
+import MenuClosed from '../../assets/Menu/healthicons_ui-menu (1).png';
 
 const Menu = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -11,7 +13,10 @@ const Menu = () => {
 
     return (
         <>
-            <button className={styles['menu-button']} onClick={toggleMenu}>Menu</button>
+            <button className={isMenuOpen ? 'menu-open' : 'menu-closed'} onClick={toggleMenu}>
+                <img src={isMenuOpen ? MenuOpen : MenuClosed} alt="Menu Icon"/>
+            </button>
+
             {isMenuOpen &&
                 <div className={styles['menu-container']}>
                     <ul className={`${styles['menu-dropdown']} ${isMenuOpen ? styles.show : ''}`}>
