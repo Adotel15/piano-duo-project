6521e02026a6d02af4305a26472979a49a294594
Gabriela <gabrielabuilcl@gmail.com>
2024-12-01 18:49:44 +0100

    PD-86968yzt0 :  contact changes added: onChange common function, event type, common useState function

--- web/src/pages/Contact/Contact.tsx
+++ web/src/pages/Contact/Contact.tsx
@@ -4,21 +4,37 @@ import imageContact from '../../assets/Contact/Concerts_017.png';
 import React, {useState} from 'react';
 
 const Contact = () => {
-    const [name, setName] = useState('');
-    const [email, setEmail] = useState('');
-    const [subject, setSubject] = useState('');
-    const [message, setMessage] = useState('');
-    const [terms, setTerms] = useState(false);
+    type Event = React.ChangeEvent<
+    HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement
+    >;
+
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        subject: '',
+        message: '',
+        terms: false,
+    });
 
     const handleSubmit = () => {
-        if (!name || !email || !subject || !message){
+        if (!formData.name || !formData.email || !formData.message || !formData.subject){
             alert('Completa todos los campos.');
             return;
         }
-        if (!terms){
+        if (!formData.terms){
             alert('Debes aceptar los términos y condiciones.');
             return;
         }
+        alert('Formulario enviado');
+    };
+
+    const onChange = (event: Event) => {
+        const target = event.target as HTMLInputElement;
+        const {name, value, type, checked } = target;
+        setFormData(prevState => ({
+            ...prevState,
+            [name]: type === 'checkbox' ? checked : value,
+        }));
     };
 
     return (
@@ -34,21 +50,21 @@ const Contact = () => {
                     <h1>—Contacto</h1>
                     <form className={styles['contact-form-container']} onSubmit={handleSubmit}>
                         <label htmlFor="name">Nombre</label>
-                        <input className={styles['input-form']} id="name" type="text" value={name} onChange={e => setName(e.target.value)}/>
+                        <input className={styles['input-form']} name='name' id="name" type="text" onChange={e => onChange(e)}/>
 
                         <label htmlFor="email">Correo electrónico</label>
-                        <input className={styles['input-form']} id="email" type="text" value={email} onChange={e => setEmail(e.target.value)} />
+                        <input className={styles['input-form']} name='email' id="email" type="text" onChange={e => onChange(e)} />
 
                         <label htmlFor="subject">Asunto</label>
-                        <input className={styles['input-form']} id="subject" type="text" value={subject} onChange={e => setSubject(e.target.value)}/>
+                        <input className={styles['input-form']} name='subject' id="subject" type="text" onChange={e => onChange(e)}/>
 
                         <label htmlFor="message">Mensaje
-                            <textarea className={[styles['input-form'], styles['contact-textarea']].join(' ')} id="message" rows={7} value={message} onChange={e => setMessage(e.target.value)}/>
+                            <textarea className={[styles['input-form'], styles['contact-textarea']].join(' ')} id="message" rows={7} onChange={e => onChange(e)}/>
                         </label>
 
                         <div className={styles['terms-and-conditions-container']}>
                             <div className={styles['checkbox-container']}>
-                                <input id="checkbox" type="checkbox" className={styles['checkbox-style']} checked={terms} onChange={e => setTerms(e.target.checked)}/>
+                                <input name='terms' id="checkbox" type="checkbox" className={styles['checkbox-style']} checked={formData.terms} onChange={e => onChange(e)}/>
                             </div>
                             <label>
                                 He leído y acepto los <span>términos y condiciones</span> y acepto la <span>política de privacidad</span>
