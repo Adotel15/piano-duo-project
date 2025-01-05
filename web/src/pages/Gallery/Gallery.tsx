9dbd970cdde1da6eb203eaf002af44a2095ddd5d
Gabriela Buil <134524103+Garbolin@users.noreply.github.com>
2025-01-05 12:58:51 +0100

    PD-8696zxmh2 :  css, base structure with fetch and FormatStrapiData added for Gallery. Strapi collection added for Gallery, but is not working

--- web/src/pages/Gallery/Gallery.tsx
+++ web/src/pages/Gallery/Gallery.tsx
@@ -1,12 +1,42 @@
-import './Gallery.module.css';
+import { useState, useEffect } from 'react';
 import Navbar from '../../components/Navbar/NavBar';
 import Footer from '../../components/footer/Footer';
+import Header from '../../components/Header/Header';
+import styles from './Gallery.module.css';
+
+type Photo = {
+    id: string,
+    image: string,
+}
 
 const Gallery = () => {
+    const [photos, setPhotos] = useState<Photo[]>([]);
+
+    const getPhotos = async () => {
+        const response = await fetch('http://localhost:8081/v1/api/photos');
+        const { data } = await response.json();
+        setPhotos(data);
+    };
+
+    useEffect(() => {
+        getPhotos();
+    }, []);
+
     return (
         <div>
             <Navbar/>
-            <h1>Galería</h1>
+            <div className={styles['gallery-page-container']}>
+                <div className={styles['gallery-header-container']}>
+                    <Header content='—Galería'></Header>
+                </div>
+                <div className={styles['gallery-container']}>
+                    {photos.map(photo =>
+                        <div key={photo.id} className={styles['photos-container']}>
+                            <img className={styles['gallery-photo']} src={photo.image} alt="" />
+                        </div>
+                    )}
+                </div>
+            </div>
             <Footer/>
         </div>
     );
