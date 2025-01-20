9a0250772a394873a4197d67002668443d8cf4ba
Gabriela Buil <134524103+Garbolin@users.noreply.github.com>
2025-01-20 20:46:33 +0100

    PD-8697afh50 : Repertoire css structure completed

--- web/src/pages/Repertoire/Repertoire.tsx
+++ web/src/pages/Repertoire/Repertoire.tsx
@@ -2,7 +2,10 @@ import styles from './Repertoire.module.css';
 import Navbar from '../../components/Navbar/NavBar';
 import Footer from '../../components/footer/Footer';
 import Header from '../../components/Header/Header';
+
+import ReperoireImage from '../../assets/Repertoire/Concerts_019.png';
 import { useState, useEffect } from 'react';
+import React from 'react';
 
 type Repertoire = {
     id : string,
@@ -27,36 +30,48 @@ const Repertoire = () => {
     };
 
     return (
-        <div>
+        <div className={styles['repertoire-page-container']}>
             <Navbar/>
             <div className={styles['header-repertoire-container']}>
                 <Header content='—Repertorio'></Header>
             </div>
-            <div>
-                {Repertoires.map(Repertoires => {
-                    return (
-                        <>
-                            <div key={Repertoires.id}>
-                                <div>{Repertoires.title}</div>
-                            </div>
-                            <div>
-                                {Repertoires.piece_author.map(piece_author => {
-                                    return (
-                                        <div>
-
-                                            <div>
-                                                {piece_author.title}
+            <div className={styles['repertoire-content-container']}>
+                <div className={styles['left-repertoire-container']}>
+                    <div className={styles['repertoire-menu-container']}>
+                        {Repertoires.map(Repertoires => {
+                            return (
+                                <div key={Repertoires.id}>
+                                    <p className={styles['repertoire_menu']}>{Repertoires.title}</p>
+                                </div>
+                            );
+                        })}
+                    </div>
+                    <div className={styles['image-repertorie-conatiner']}>
+                        <img className={styles['image-repertorie']} src={ReperoireImage} alt="Repertoire" />
+                    </div>
+                </div>
+                <div className={styles['right-repertorie-conatiner']}>
+                    {Repertoires.map(Repertoires => {
+                        return (
+                            <div className={styles['repertories-conatiner']}>
+                                <div className={styles['repertorie-title-conatiner']} key={Repertoires.id}>
+                                    <div>{Repertoires.title}</div>
+                                </div>
+                                <div className={styles['piece-author-content-conatiner']}>
+                                    {Repertoires.piece_author.map(piece_author => {
+                                        return (
+                                            <div className={styles['piece-author-conatiner']}>
+                                                <p>{piece_author.title}</p>
+                                                <p className={styles['line-repertoire']}></p>
+                                                <p>{piece_author.author}</p>
                                             </div>
-                                            <div>
-                                                {piece_author.author}
-                                            </div>
-                                        </div>
-                                    );
-                                })}
+                                        );
+                                    })}
+                                </div>
                             </div>
-                        </>
-                    );
-                })}
+                        );
+                    })}
+                </div>
 
             </div>
             <Footer/>
