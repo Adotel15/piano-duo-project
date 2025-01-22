a93b6ad47d5d5028ee82656a80bf012ba0f17129
Gabriela Buil <134524103+Garbolin@users.noreply.github.com>
2025-01-22 21:42:39 +0100

    PD-8696zxn9x : toggle front-back CD image added

--- web/src/pages/Music/Music.tsx
+++ web/src/pages/Music/Music.tsx
@@ -4,6 +4,7 @@ import Header from '../../components/Header/Header';
 import Footer from '../../components/footer/Footer';
 
 //Assets
+import toggleImage from '../../assets/Cds/material-symbols_flip-camera-android-rounded.png';
 // import FrontCD1 from '../../assets/Cds/DVD_02.png';
 // import FrontCD2 from '../../assets/Cds/DVD_05.png';
 // import FrontCD3 from '../../assets/Cds/DVD_13.png';
@@ -27,15 +28,40 @@ type CD = {
 
 const Music = () => {
     const [CDs, setCDs] = useState <CD[]> ([]);
+    const [visibleStates, setVisibleStates] = useState<{ [id: string]: boolean }>(
+        {}
+    );
+
+    useEffect(() => {
+        const initialState = CDs.reduce(
+            (acc: { [id: string]: boolean }, cd: CD) => {
+                acc[cd.id] = true; // Assume frontImage is visible initially
+                return acc;
+            },
+            {}
+        );
+        setVisibleStates(initialState);
+    }, [CDs]);
+
+    const handleToggle = (id: string) => {
+        setVisibleStates(prev => ({
+            ...prev,
+            [id]: !prev[id], // Invierte el estado de visibilidad para el CD correspondiente
+        }));
+    };
 
     useEffect(() => {
         getCDs();
     }, []);
 
     const getCDs = async () => {
-        const response = await fetch('http://localhost:8081/v1/api/cds');
-        const { data } = await response.json();
-        setCDs(data);
+        try{
+            const response = await fetch('http://localhost:8081/v1/api/cds');
+            const { data } = await response.json();
+            setCDs(data);
+        } catch (error) {
+            console.error('Error fetching CDs:' , error);
+        }
     };
 
     return (
@@ -46,16 +72,31 @@ const Music = () => {
             </div>
             <div className={styles['sections-container']}>
                 {CDs.map(cd => {
+                    const isFrontVisible = visibleStates[cd.id];
                     return (
                         <section key={cd.id} className={styles['section-cd']}>
                             <div className= {styles['images-container']}>
-                                {cd.frontImage && <div className={styles['CD-front-image-container']}>
-                                    <img className={styles['CD-image']} src={cd.frontImage} alt="CD 1" />
-                                </div>
+                                {cd.frontImage ?
+                                    <>
+                                        <div className={styles['toggle-image-container']} onClick={() => handleToggle(cd.id)} >
+                                            <img className={styles['toggle-image']} src={toggleImage} />
+                                        </div>
+                                        <div className={`${styles['CD-front-image-container']} ${
+                                            isFrontVisible ? styles['show'] : styles['hide']
+                                        }`}>
+                                            <img className={styles['CD-image']} src={cd.frontImage} alt="CD 1" />
+                                        </div>
+                                        <div className={`${styles['CD-back-image-container']} ${
+                                            isFrontVisible ? styles['hide'] : styles['show']
+                                        }`}>
+                                            <img className={styles['CD-image']} src={cd.backImage} alt="CD 1" />
+                                        </div>
+                                    </>
+                                    :
+                                    <div className={styles['CD-back-image-containe-only']}>
+                                        <img className={styles['CD-image']} src={cd.backImage} alt="CD 1" />
+                                    </div>
                                 }
-                                <div className={styles['CD-back-image-container']}>
-                                    <img className={styles['CD-image']} src={cd.backImage} alt="CD 1" />
-                                </div>
                             </div>
 
                             <div className={styles['content-container']}>
