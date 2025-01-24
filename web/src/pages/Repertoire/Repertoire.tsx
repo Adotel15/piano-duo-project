f31398e4139a7cc9c8b9d799b602b1df925e21d9
Adotel15 <adriandotel@gmail.com>
2025-01-24 18:20:30 +0100

    PD-8697afh50 Minor structure fixes

--- web/src/pages/Repertoire/Repertoire.tsx
+++ web/src/pages/Repertoire/Repertoire.tsx
@@ -1,11 +1,12 @@
-import styles from './Repertoire.module.css';
+import { useState, useEffect } from 'react';
+
 import Navbar from '../../components/Navbar/NavBar';
 import Footer from '../../components/footer/Footer';
 import Header from '../../components/Header/Header';
 
+import styles from './Repertoire.module.css';
+
 import ReperoireImage from '../../assets/Repertoire/Concerts_019.png';
-import { useState, useEffect } from 'react';
-import React from 'react';
 
 type Repertoire = {
     id : string,
@@ -17,7 +18,7 @@ type Repertoire = {
 }
 
 const Repertoire = () => {
-    const [Repertoires, setRepertoires] = useState <Repertoire[]> ([]);
+    const [repertoires, setRepertoires] = useState <Repertoire[]> ([]);
     const [selectedId, setSelectedId] = useState('1');
 
     useEffect(() => {
@@ -25,15 +26,20 @@ const Repertoire = () => {
     }, []);
 
     const getRepertoires = async () => {
-        const response = await fetch('http://localhost:8081/v1/api/repertoires');
-        const { data } = await response.json();
-        setRepertoires(data);
+        try {
+            const response = await fetch('http://localhost:8081/v1/api/repertoires');
+            const { data } = await response.json();
+            setRepertoires(data);
 
-        const defaultRepertoire = data.find((rep: Repertoire) => rep.id === '1');
-        if (defaultRepertoire) {
-            setSelectedId(defaultRepertoire.id);
-        } else if (data.length > 0) {
-            setSelectedId(data[0].id);
+            const defaultRepertoire = data.find((rep: Repertoire) => rep.id === '1');
+            if (defaultRepertoire) {
+                setSelectedId(defaultRepertoire.id);
+            } else if (data.length > 0) {
+                setSelectedId(data[0].id);
+            }
+        } catch (error) {
+            // eslint-disable-next-line no-console
+            console.log('Error fetching repertoires', error);
         }
     };
 
@@ -46,7 +52,7 @@ const Repertoire = () => {
             <div className={styles['repertoire-content-container']}>
                 <div className={styles['left-repertoire-container']}>
                     <div className={styles['repertoire-menu-container']}>
-                        {Repertoires.map(repertoire => {
+                        {repertoires.map(repertoire => {
                             return (
                                 <div key={repertoire.id}
                                     onClick={() => setSelectedId(repertoire.id)} defaultValue={'1'}
@@ -66,14 +72,14 @@ const Repertoire = () => {
                 </div>
                 <div className={styles['right-repertorie-conatiner']}>
                     {selectedId &&
-                    Repertoires.filter(repertoire => repertoire.id === selectedId).map(Repertoires => {
+                    repertoires.filter(repertoire => repertoire.id === selectedId).map(repertoire => {
                         return (
                             <div className={styles['repertories-conatiner']}>
-                                <div className={styles['repertorie-title-conatiner']} key={Repertoires.id}>
-                                    <div>{Repertoires.title}</div>
+                                <div className={styles['repertorie-title-conatiner']} key={repertoire.id}>
+                                    <div>{repertoire.title}</div>
                                 </div>
                                 <div className={styles['piece-author-content-conatiner']}>
-                                    {Repertoires.piece_author.map(piece_author => {
+                                    {repertoire.piece_author.map(piece_author => {
                                         return (
                                             <div className={styles['piece-author-conatiner']}>
                                                 <p>{piece_author.title}</p>
