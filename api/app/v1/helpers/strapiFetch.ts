ef795241ce1c554e0c84ff113de641a627801d8a
Adotel15 <adriandotel@gmail.com>
2025-03-02 17:49:27 +0100

    PD-00000000 Logs

--- api/app/v1/helpers/strapiFetch.ts
+++ api/app/v1/helpers/strapiFetch.ts
@@ -4,6 +4,8 @@ import { STRAPI_URL, STRAPI_TOKEN } from '../constants/env.js';
 import type { StrapiEndpoints } from '../../types/strapi.js';
 
 export const strapiFetch = async (collection: StrapiEndpoints, language:string, id?: string): Promise<unknown> => {
+    console.log(`[GET ALL] STRAPI_URL:, ${STRAPI_URL}`);
+    console.log(`[GET ALL] STRAPI_TOKEN:, ${STRAPI_TOKEN}`);
     const path = id ?
         `${STRAPI_URL}/${collection as string}/${id}?populate=*&locale=${language}`
         : `${STRAPI_URL}/${collection as string}?populate=*&locale=${language}`;
